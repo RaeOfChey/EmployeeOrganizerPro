ebd7c0f06058f224633f0dcba3a2121435008b06
RaeofChey <cheyennaraelynn@gmail.com>
2024-10-20 16:01:21 -0500

    Added initial functions to JavaScript file

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Employee Organizer Pro
+
+![License](https://img.shields.io/badge/license-MIT-brightgreen.svg)
+
+## Table of Contents
+1. [Description](#description)
+2. [Features](#features)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [Walkthrough](#walkthrough)
+6. [License](#license)
+7. [Contributing](#contributing)
+8. [Tests](#tests)
+9. [Questions](#questions)
+
+## Description
 Employee Organizer Pro is a command-line application to manage company employees, departments, and roles with ease! Built using Node.js, Inquirer, and PostgreSQL, this tool allows users to view, add, and update employee data, helping business owners efficiently organize and plan their workforce.
+
+## Features
+Coming soon
+
+## Installation
+Coming soon
+
+## Usage
+Coming soon
+
+## Walkthrough
+A complete walkthrough video demonstrating the application will be available soon.
+
+## License
+This project is licensed under the MIT License, which allows you to freely use, modify, and distribute this software, provided proper attribution is given.
+
+## Contributing
+Contributions are welcome!  If you'd like to contribute to this project, follow the steps below.
+
+- Step 1: Fork the repository.
+- Step 2: Create a feature branch: git checkout -b feature/new-feature
+- Step 3: Commit your changes: git commit -m "Add new feature"
+- Step 4: Push to the branch: git push origin feature/new-feature
+- Step 5: Open a pull request.
+
+## Tests
+Currently, this project does not have any automated tests. However, if you'd like to add tests, feel free to contribute! You can set up testing using a framework like Mocha or Jest.
+
+To run any test files in the future, use: npm test
+
+## Questions
+If you have any questions about the repository, feel free to reach out by opening an issue or contacting me directly at cheyennaraelynn@gmail.com You can also find more of my work on GitHu at https://github.com/RaeOfChey.
---
+++ index.js
@@ -0,0 +1,242 @@
+const inquirer = require('inquirer'); // Imports inquirer
+const db = require('./db'); // Imports the database connection
+
+function mainMenu() {
+    console.log("Starting main menu...");
+    inquirer.default.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit',
+            ],
+        },
+    ])
+        .then((answers) => {
+            console.log(answers);
+            switch (answers.action) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    db.end(); // Closes the database connection
+                    console.log('Goodbye!');
+                    process.exit();
+            }
+        });
+}
+
+// Function to view all departments
+function viewDepartments() {
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu(); // Return to main menu
+    });
+}
+
+// Add functions for the other actions
+
+// Add viewRoles()
+function viewRoles() {
+    db.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu();
+    });
+}
+
+// Add viewEmployees()
+function viewEmployees() {
+    db.query(`
+      SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
+      CONCAT(m.first_name, ' ', m.last_name) AS manager
+      FROM employee e
+      LEFT JOIN role r ON e.role_id = r.id
+      LEFT JOIN department d ON r.department_id = d.id
+      LEFT JOIN employee m ON e.manager_id = m.id
+    `, (err, res) => {
+        if (err) throw err;
+        console.table(res.rows);
+        mainMenu();
+    });
+}
+
+// Add addDepartment()
+function addDepartment() {
+    inquirer.default.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter the name of the new department:',
+        },
+    ])
+        .then((answer) => {
+            db.query('INSERT INTO department (name) VALUES ($1)', [answer.departmentName], (err, res) => {
+                if (err) throw err;
+                console.log('Department added!');
+                mainMenu();
+            });
+        });
+}
+
+// Add addRole()
+function addRole() {
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        const departments = res.rows.map((department) => ({
+            name: department.name,
+            value: department.id,
+        }));
+
+        inquirer.default.prompt([
+                {
+                    type: 'input',
+                    name: 'roleTitle',
+                    message: 'Enter the title of the new role:',
+                },
+                {
+                    type: 'input',
+                    name: 'roleSalary',
+                    message: 'Enter the salary for the new role:',
+                },
+                {
+                    type: 'list',
+                    name: 'departmentId',
+                    message: 'Choose a department for this role:',
+                    choices: departments,
+                },
+            ])
+            .then((answers) => {
+                db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [answers.roleTitle, answers.roleSalary, answers.departmentId], (err, res) => {
+                    if (err) throw err;
+                    console.log('Role added!');
+                    mainMenu();
+                });
+            });
+    });
+}
+
+// Add addEmployee()
+function addEmployee() {
+    db.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        const roles = res.rows.map((role) => ({
+            name: role.title,
+            value: role.id,
+        }));
+
+        db.query('SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            const managers = res.rows.map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id,
+            }));
+
+            inquirer.default.prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: 'Enter the first name of the employee:',
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'Enter the last name of the employee:',
+                    },
+                    {
+                        type: 'list',
+                        name: 'roleId',
+                        message: 'Choose a role for the employee:',
+                        choices: roles,
+                    },
+                    {
+                        type: 'list',
+                        name: 'managerId',
+                        message: 'Choose a manager for the employee (or select None):',
+                        choices: [...managers, { name: 'None', value: null }],
+                    },
+                ])
+                .then((answers) => {
+                    db.query(
+                        'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
+                        [answers.firstName, answers.lastName, answers.roleId, answers.managerId],
+                        (err, res) => {
+                            if (err) throw err;
+                            console.log('Employee added!');
+                            mainMenu();
+                        }
+                    );
+                });
+        });
+    });
+}
+
+// Add updateEmployeeRole()
+function updateEmployeeRole() {
+    db.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        const employees = res.rows.map((employee) => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id,
+        }));
+
+        db.query('SELECT * FROM role', (err, res) => {
+            if (err) throw err;
+            const roles = res.rows.map((role) => ({
+                name: role.title,
+                value: role.id,
+            }));
+
+            inquirer.default.prompt([
+                    {
+                        type: 'list',
+                        name: 'employeeId',
+                        message: 'Choose an employee to update:',
+                        choices: employees,
+                    },
+                    {
+                        type: 'list',
+                        name: 'roleId',
+                        message: 'Choose the new role for the employee:',
+                        choices: roles,
+                    },
+                ])
+                .then((answers) => {
+                    db.query('UPDATE employee SET role_id = $1 WHERE id = $2', [answers.roleId, answers.employeeId], (err, res) => {
+                        if (err) throw err;
+                        console.log('Employee role updated!');
+                        mainMenu();
+                    });
+                });
+        });
+    });
+}
+
+mainMenu(); // Starts the application
